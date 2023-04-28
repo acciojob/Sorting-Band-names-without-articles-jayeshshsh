3286e798a34ac540c06516e41543004cf9ddf47f
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-04-28 23:07:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
+//your code here
+const bandnames = ['The Beatles', 'Aerosmith', 'The Rolling Stones', 'Anthrax', 'The Who'];
 
+function strip(bandname){
+  return bandname.replace(/^(a |the |an )/i, '').trim();
+}
+
+const sortedbands = bandnames.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+const bandlist = document.querySelector('#bands');
+
+for (let i = 0; i < sortedbands.length; i++) {
+  const li = document.createElement('li');
+  li.textContent = sortedbands[i];
+  bandlist.appendChild(li);
+}
